aa2ca6236db182485b199cd064fbad12fb7ee0b4
Tran Lieu Huy Khanh <lieuhuykhanh@gmail.com>
2025-04-12 06:42:44 +0700

    Update ExamController.java

--- Nhom1_Lap_Trinh_Java_Web/WebOnThiTracNghiem/src/main/java/com/example/WebOnThiTracNghiem/controller/ExamController.java
+++ Nhom1_Lap_Trinh_Java_Web/WebOnThiTracNghiem/src/main/java/com/example/WebOnThiTracNghiem/controller/ExamController.java
@@ -10,10 +10,7 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.File;
@@ -47,7 +44,7 @@ public class ExamController {
         return "Exams/Add-Exam";
     }
     @PostMapping("/admin/exams/add")
-    public String addExam(@Valid Exam exam, BindingResult result, Model model) {
+    public String addExam(@Valid @ModelAttribute Exam exam, BindingResult result, Model model) {
         if (result.hasErrors()) {
             return "redirect:/admin/exams/add";
         }
